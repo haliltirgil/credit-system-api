9e1dc7f9017949739c010f260c5d78ebcd0c5076
haliltirgil <tirgil18@gmail.com>
2023-12-03 14:43:47 +0300

    Add credit router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express, { Router } from 'express';
 import { userRouter } from './user';
+import { creditRouter } from './credit';
 
 const router: Router = express.Router();
 
 router.use('/users', userRouter);
+
+router.use('/credits', creditRouter);
 
 export { router };
