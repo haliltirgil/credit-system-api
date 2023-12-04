892043d7d3a0c8843a647edb0fe554a64f8c7818
haliltirgil <tirgil18@gmail.com>
2023-12-04 22:17:02 +0300

    Add take credit logic and partial pay credit logic

--- src/controllers/credit.ts
+++ src/controllers/credit.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from 'express';
 import { User } from '../models/user';
+import { NotFoundError } from '../errors/not-found-error';
+import { Credit } from '../models/credit';
+import { CreditStatus, InstallmentStatus } from '../enums/enum-variables';
+import { Installment } from '../models/installment';
+import { calculateMonthWeekdays } from '../utils/calculate-weekdays';
+import { calculateInstallments } from '../utils/calculate-installments';
+import { ForbiddenError } from '../errors/forbidden-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 export class CreditController {
   public static async takeCredit(req: Request, res: Response): Promise<void> {
@@ -10,50 +18,96 @@ export class CreditController {
     });
 
     if (!user) {
-      throw new Error('');
+      throw new NotFoundError('Error', 'User not found!');
     }
-    /**
-     * bodyi al
-     *
-     * kontrolleri yap
-     *
-     * tarihleri hesaplayan fonksiyonu yaz
-     *
-     * credit oluştur
-     *
-     * installments oluştur
-     *
-     * transaction kullan
-     *
-     * response dön
-     *
-     */
-    res.status(201).send({ message: 'Welcome from take credit endpoint' });
+
+    const credit = new Credit();
+    credit.status = CreditStatus.Approved;
+    credit.amount = amount;
+    credit.installmentCount = installmentCount;
+    credit.user = userId;
+    await credit.save();
+
+    const installmentAmounts = calculateInstallments(amount, installmentCount);
+    const installmentDates = calculateMonthWeekdays(new Date(), installmentCount);
+
+    for (let i = 0; i < installmentCount; i++) {
+      const installment = new Installment();
+      installment.amount = installmentAmounts[i];
+      installment.status = InstallmentStatus.NotPaid;
+      installment.credit = credit;
+      installment.dueDate = installmentDates[i];
+
+      // eslint-disable-next-line no-await-in-loop
+      await installment.save();
+    }
+
+    const installments = await Installment.find({ where: { credit: { id: credit.id } } });
+
+    res.status(201).send({
+      message: 'Credit created successfully!',
+      result: {
+        creditId: credit.id,
+        installments,
+      },
+    });
   }
 
-  public static async getUserCreditByStatus(_req: Request, res: Response): Promise<void> {
-    /**
-     * user var mı yok mu kontrol et
-     *
-     * user varsa credits tablosundan userIdye göre filtreleyip kredilerini statülerine göre listele
-     *
-     * tarih nasıl isteniyor öğren
-     */
-    res.status(201).send({ message: 'Welcome from get user credits by status endpoint' });
+  public static async getUserCreditByStatus(req: Request, res: Response): Promise<void> {
+    const { userId } = req.params;
+
+    const user = await User.findOneBy({
+      id: Number(userId),
+    });
+
+    if (!user) {
+      throw new NotFoundError('Error', 'User not found!');
+    }
+
+    // TODO: edit this query
+    const result = await Credit.find({ where: { status: 0 } });
+
+    res.status(200).send(result);
   }
 
-  public static async repayCreditInstallment(_req: Request, res: Response): Promise<void> {
-    /**
-     * gelen bodyde ya da parametre olarak userId alacağım installment id alacağım
-     *
-     * userId üzerinden credit oradan installmenta gideceğim
-     *
-     * eğer hatalı caseler olursa error throw edeceğim
-     *
-     * taksiti tamamen ödeme mi yapacak parçalı mı onu belirleyeceğiz
-     *
-     * taksit ödendiğinde o taksitin statusu değişmeli
-     */
-    res.status(201).send({ message: 'Welcomefrom repay endpoint' });
+  public static async repayCreditInstallment(req: Request, res: Response): Promise<void> {
+    const { userId } = req.params;
+    const { installmentId, amount } = req.body;
+
+    const user = await User.findOneBy({
+      id: Number(userId),
+    });
+
+    if (!user) {
+      throw new NotFoundError('Error', 'User not found!');
+    }
+
+    const installment = await Installment.findOneBy({ id: installmentId });
+
+    if (!installment) {
+      throw new NotFoundError('Error', 'Installment not found!');
+    }
+
+    if (!user.credits.includes(installment.credit)) {
+      throw new ForbiddenError();
+    }
+
+    if (amount > installment.amount) {
+      throw new BadRequestError('Error', 'Payment cannot be greater than the debt!');
+    }
+
+    if (amount < installment.amount) {
+      installment.status = InstallmentStatus.PartialPaid;
+      installment.amount -= amount;
+
+      await installment.save();
+
+      res.status(200).send({ message: 'Partial payment successful!', result: installment });
+    }
+
+    installment.status = InstallmentStatus.Paid;
+    await installment.save();
+
+    res.status(200).send({ message: 'Payment successful!' });
   }
 }
