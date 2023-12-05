341b5759ea31c44296ed244ff4290e8e384edd71
haliltirgil <tirgil18@gmail.com>
2023-12-05 15:05:25 +0300

    Edit outdated installment logic

--- src/services/installment-service.ts
+++ src/services/installment-service.ts
@@ -1,5 +1,6 @@
 import { LessThan } from 'typeorm';
 import { Installment } from '../models/installment';
+import { INTEREST_VALUE, OVERDATE } from '../constants/const-variables';
 
 /**
  * Service class for managing installment-related operations.
@@ -14,11 +15,16 @@ export class InstallmentService {
    * @param currentDate - The current date used to find out-of-date installments.
    *
    * @returns A Promise that resolves to an array of out-of-date installments.
-   * TOOD: write logic
    */
   static async findOutOfDateInstallments(currentDate: Date) {
     const installments = await Installment.findBy({ dueDate: LessThan(currentDate) });
 
-    return installments;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const installment of installments) {
+      installment.totalInterest = (OVERDATE * INTEREST_VALUE * installment.remainingTotalAmount) / 360;
+
+      // eslint-disable-next-line no-await-in-loop
+      await installment.save();
+    }
   }
 }
