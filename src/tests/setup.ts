f9110d08b2e01774da0a7deaa4591373889fd716
haliltirgil <tirgil18@gmail.com>
2023-12-05 19:09:43 +0300

    Edit configurations

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -15,6 +15,21 @@ beforeAll(async () => {
   }
 });
 
+afterEach(async () => {
+  await source.query(`CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+  DECLARE
+      statements CURSOR FOR
+          SELECT tablename FROM pg_tables
+          WHERE tableowner = username AND schemaname = 'public';
+  BEGIN
+      FOR stmt IN statements LOOP
+          EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+      END LOOP;
+  END;
+  $$ LANGUAGE plpgsql`);
+  await source.query(`SELECT truncate_tables('MYUSER')`);
+});
+
 afterAll(async () => {
   await source.dropDatabase();
   await source.destroy();
