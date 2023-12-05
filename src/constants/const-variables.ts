e1c4dc9d167f0d5e719c85bca5408bdabea695bb
haliltirgil <tirgil18@gmail.com>
2023-12-05 15:03:59 +0300

    Add interest variables

--- src/constants/const-variables.ts
+++ src/constants/const-variables.ts
@@ -3,4 +3,7 @@ import { NotFoundError } from '../errors/not-found-error';
 export const MIN_LENGTH = 1;
 export const MAX_LENGTH = 100;
 
+export const INTEREST_VALUE = 1.5;
+export const OVERDATE = 1;
+
 export const CUSTOM_ERRORS = [NotFoundError];
