4168a7c81565128e19daa2ad0d96b0393ae9fe3b
haliltirgil <tirgil18@gmail.com>
2023-12-05 15:37:57 +0300

    Add validation for query parameters

--- src/validations/credit-validation.ts
+++ src/validations/credit-validation.ts
@@ -1,10 +1,17 @@
+import { queryDate } from './custom-validations/date-validation';
 import { bodyId, paramId } from './custom-validations/id-validation';
-import { bodyFloat, bodyNumber } from './custom-validations/number-validation';
+import { bodyFloat, bodyNumber, queryNumber } from './custom-validations/number-validation';
 
 export class CreditValidation {
   public static takeCredit = [bodyId('userId'), bodyFloat('amount'), bodyNumber('installmentCount')];
 
-  public static getUserCreditByStatus = [paramId('userId')];
+  public static getUserCreditByFilters = [
+    queryNumber('status').isInt({ min: 0 }).withMessage(`Parameter "status" must be 0 or greater then equal 0.`),
+    queryNumber('limit').isInt({ min: 1 }).withMessage(`Parameter "limit" must be 1 or greater then equal 1.`),
+    queryNumber('page').isInt({ min: 1 }).withMessage(`Parameter "page" must be 1 or greater then equal 1.`),
+    queryDate('startDate'),
+    paramId('userId'),
+  ];
 
   public static repayCreditInstallment = [paramId('userId'), bodyId('installmentId'), bodyFloat('amount')];
 }
